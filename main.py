89b748926da149a57a5c483ff7cc2825ede00606
sebastian <sebastian.luraschi8@gmail.com>
2020-06-15 17:07:43 +0100

    remove renaming of sent file

--- main.py
+++ main.py
@@ -12,10 +12,5 @@ if __name__ == "__main__":
         print("Starting to publish file:", file_name, "...")
         mqtt_publisher.publish(file_name)
         print("OK!")
-        now = datetime.now()
-        dt_string = now.strftime("%d_%m_%Y-%H%M%S")
-        print("Moving file...")
-        os.rename(file_name, "/home/pi/package-storage/" + "[" + dt_string + "]-" + file_name)
-#    os.rename(file_name, "/Users/sebastianluraschi/Documents/Repositories/TP_prof/Mqtt_publisher" + "[" + dt_string + "]-" + file_name)
-        print("OK!")
+
 
